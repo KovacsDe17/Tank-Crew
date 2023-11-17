9d45a12cc0a72143a22dc8920bde7305a89983f5
Daniel Kovacs <kovacs.de17@gmail.com>
2023-11-17 07:31:08 +0100

    Bug Fix on Network Manager
    Set to delete the instance on leaving the multiplayer scene

--- Assets/Scripts/UI/Helpers/DestroyNetworkManager.cs
+++ Assets/Scripts/UI/Helpers/DestroyNetworkManager.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Services.Authentication;
 using UnityEngine;
-using UnityEngine.UI;
 
 /// <summary>
 /// This script ensures, that leaving the multiplayer section removes the Network Manager Singleton.
@@ -10,11 +8,23 @@ using UnityEngine.UI;
 /// </summary>
 public class DestroyNetworkManager : MonoBehaviour
 {
-    private void Awake()
+    [SerializeField] private int _MultiplayerSceneID;
+
+    private void OnLevelWasLoaded(int level)
     {
-        GetComponent<Button>().onClick.AddListener(() =>
-        {
-            Destroy(NetworkManager.Singleton.gameObject);
-        });
+        if(level != _MultiplayerSceneID)
+            DestroyNetworkManagerInstance();
+    }
+
+    /// <summary>
+    /// Shut down the Network Manager and destroy it's singleton instance
+    /// </summary>
+    private void DestroyNetworkManagerInstance()
+    {
+        AuthenticationService.Instance.SignOut();
+
+        NetworkManager.Singleton.Shutdown();
+        
+        Destroy(NetworkManager.Singleton.gameObject);
     }
 }
